f8a83ed4b246066c69575cebf294c607a7451faf
Julia Carlberg <juliacarlbrg@gmail.com>
2022-12-03 16:30:36 +0100

    another commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,42 @@
 import { Todo } from "./Todo";
 
-
 let todoList= [
   new Todo("Plugga", false),
   new Todo("Städa", false),
   new Todo("Äta", false),
-]
+];
 
 function start(){
-    let ulTag= document.getElementById("theList");
+  let ulTag= document.getElementById("theList");
 
-    for(let i=0; i<todoList.length; i++){
-        let liTag= document.createElement("li");
-        liTag.innerHTML= (todoList[i].todo);
-        ulTag.appendChild(liTag);
-    }
-}
+  for(let i=0; i<todoList.length; i++){
+    let liTag= document.createElement("li");
+    liTag.innerHTML= (todoList[i].todo);
+    ulTag.appendChild(liTag);
+  }
+};
 
 start();
 
 
 function removeTask(){
-    let myCloseList= document.getElementsByTagName("li");
-    for(i=0; i<myCloseList.length; i++){
-      let closeButton= document.createElement("BUTTON");
-      closeButton.className="close";
-      myCloseList[i].appendChild(closeButton);
-      closeButton.innerHTML="&#10005;";
-    } 
+  let myCloseList= document.getElementsByTagName("li");
+  for(i=0; i<myCloseList.length; i++){
+    let closeButton= document.createElement("BUTTON");
+    closeButton.className="close";
+    myCloseList[i].appendChild(closeButton);
+    closeButton.innerHTML="&#10005;";
+  } 
 
-    let close= document.getElementsByClassName("close");
-    for(i=0; i<close.length; i++){
-        close[i].onclick= function(){
-            let div= this.parentElement;
-            div.style.display= "none";
-        }
+  let close= document.getElementsByClassName("close");
+  for(i=0; i<close.length; i++){
+    close[i].onclick= function(){
+      let div= this.parentElement;
+      div.style.display= "none";
     }
+  }
 
-}
+};
 
 
 removeTask();
@@ -46,21 +45,27 @@ function addTask(){
   let inputTag= document.getElementById("input");
   let ulTag= document.getElementById("theList");
   let liTag= document.createElement("li");
-  liTag.innerHTML= (inputTag.value);
-  ulTag.appendChild(liTag);
+  let addButton= document.getElementById("addButton")
   let closeButton= document.createElement("BUTTON");
-  closeButton.className="close";
-  liTag.appendChild(closeButton);
-  closeButton.innerHTML="&#10005;";
-  let close= document.getElementsByClassName("close");
-  for(i=0; i<close.length; i++){
-    close[i].onclick= function(){
-      let div= this.parentElement;
-      div.style.display= "none";
-    }
-  }
-}
 
+  addButton.addEventListener("click", ()=>{
+    ulTag.appendChild(liTag);
+    liTag.innerHTML=(inputTag.value);
+    closeButton.className="close";
+    liTag.appendChild(closeButton);
+    closeButton.innerHTML="&#10005;";
+    let close= document.getElementsByClassName("close");
+    for(i=0; i<close.length; i++){
+      close[i].onclick= function(){
+        let div= this.parentElement;
+        div.style.display= "none";
+      };
+    };
+  });
+  
+};
+
+addTask();
 
 let list = document.querySelector("ul");
 list.addEventListener("click", function(ev) {
@@ -68,3 +73,9 @@ list.addEventListener("click", function(ev) {
     ev.target.classList.toggle("checked");
   }
 }, false);
+
+let liTag= document.getElementsByTagName("li");
+
+liTag.addEventListener("click", ()=>{
+  
+})
